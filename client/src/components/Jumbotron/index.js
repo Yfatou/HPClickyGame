8d9b3535053e744a4c3a961b5a4b2da807905539
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2019-03-09 23:12:06 -0600

    deployed

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Jumbotron = () => (
+	<header className = "header">
+		<h1>Harry Potter Clicky Game!</h1>
+		<h2>Click on any image to earn a point, but don't click on any more than once!</h2>
+	</header>
+);
+
+
+export default Jumbotron;
---
+++ src/components/FriendCard/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    <div className="card"
+      onClick={() => props.schuffleFriend(props.id)}>
+      <div className="img-container">
+        <img className="img" alt={props.name} src={props.image} id={props.id}/>
+      </div>
+    </div>
+   );
+}
+
+export default FriendCard;
+
+
