e8209f7d81f3074e84e6a98e2d504c817fca040f
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2019-03-09 23:48:59 -0600

    Updates

---
+++ precache-manifest.0ec6247c010149ec6c2a8caee010fbeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f305df6c341da7d4a235852f8765ad04",
+    "url": "/HPClickyGame/static/media/navbar.f305df6c.jpg"
+  },
+  {
+    "revision": "d06490e40b350be6509874a114efb162",
+    "url": "/HPClickyGame/static/media/HP.d06490e4.jpg"
+  },
+  {
+    "revision": "235b50fa269af7163135",
+    "url": "/HPClickyGame/static/js/runtime~main.9c8d4569.js"
+  },
+  {
+    "revision": "bb61f3c0b8b3af9d8c55",
+    "url": "/HPClickyGame/static/js/main.d2dd67ab.chunk.js"
+  },
+  {
+    "revision": "ac46a3d8ec6c9fc1235b",
+    "url": "/HPClickyGame/static/js/2.00bdfcd8.chunk.js"
+  },
+  {
+    "revision": "bb61f3c0b8b3af9d8c55",
+    "url": "/HPClickyGame/static/css/main.4749adca.chunk.css"
+  },
+  {
+    "revision": "d8cfb0da9eba1fa12c8e5e38c819c824",
+    "url": "/HPClickyGame/index.html"
+  }
+];
